0e86d515f7713e1e8811bb43a97b8cdf229d619c
turanukimaru <turanukimaru@gmail.com>
2019-05-20 20:01:36 +0900

    blade damage debug, Tailtiu debug

---
+++ core/src/jp/blogspot/turanukimaru/sample/Sein.kt
@@ -0,0 +1,46 @@
+package jp.blogspot.turanukimaru.sample
+
+import java.util.*
+
+data class Sein<T>( // T が若干鬱陶しいが他の型と区別する役に立つ
+        val enum : T? = null,// Sein から enum を復元するときに使う。enum に戻す予定がないなら不要。
+        val category: Int = 0,
+        val key: Int = 0,
+        val label: String = "",
+        val start: Date = Date(), //InstantはAPIレベルが足りなかった…
+        val end: Date = Date(),
+        val time: Date? = null,//特定時刻. 時刻が指定されたときのみ値が入る
+        val header: Sein<T>? = null,//ファサードになる現在のコードを指す.　header の時のみ null
+        val prev: Sein<T>? = null//時間的に前のコードを優先度順に並べたもの。最後はnull
+) {
+
+    /**
+     * 時刻を特定しない存在（≒現在の存在）を返す。
+     */
+    fun now() = header ?: this
+
+    /**
+     * 特定時刻の存在
+     */
+    fun at(date: Date? = null): Sein<T> =
+            if (date == null) header ?: this // date が指定されなかったりnullだったりすると現在のCodeを出すようにすると使いやすい
+            else header?.dig(date) ?: dig(date) //rootから掘り進めて対象の時間のオブジェクトを探す
+
+    /**
+     * 対象の時間を含む存在を掘る.有ったら時間指定の上でコピー。最後まで存在しなかったら例外
+     */
+    private fun dig(date: Date): Sein<T> = if (date.after(start) && date.before(end)) copy(time = date) else prev?.dig(date)
+            ?: throw RuntimeException()
+
+    /**
+     * 現在時刻に即した表記を出力する.年号だけでいいときは label をそのまま出力するので十分
+     */
+    fun toTimeString(): String = if (time == null) label else "2019年の時は元年にするとか必要に応じて編集"
+
+    /**
+     * 同じカテゴリ同じキーなら別の時間でも同じコード。hash()とかも同様に同じように振舞わせる
+     */
+    override fun equals(other: Any?): Boolean = other is Sein<*> && other.category == this.category && other.key == this.key
+
+    override fun hashCode(): Int =category.hashCode() + key.hashCode()
+}
---
+++ core/src/jp/blogspot/turanukimaru/sample/時間的Enum群.kt
@@ -0,0 +1,58 @@
+package jp.blogspot.turanukimaru.sample
+
+import java.util.*
+
+//Enumがたくさんできるので適当にグループ化する
+class 時間的Enum群 {
+
+    enum class 紀年法(override val category: Int,override val key: Int) : CodeEnum<紀年法> {
+        西暦(1, 1),
+        和暦(1, 2)
+        ;
+        companion object :CodeCategory<紀年法>(1){
+            fun item(key: Int) = values().find { it.key == key }
+        }
+    }
+
+    enum class 和暦(override val category: Int,override val key: Int) : CodeEnum<和暦> {
+        平成(2, 1),
+        令和(2, 2)
+        ;
+        companion object :CodeCategory<和暦>(2){
+            fun item(key: Int) = values().find { it.key == key }
+        }
+    }
+
+}
+
+interface CodeEnum<T> {
+    val category get() = 0
+    val key get() = 0
+    //対応するコードをキャッシュから取り出して返す
+    val sein get()= Sein<T>()
+}
+
+open class CodeCategory<T>(val category: Int)  {
+    fun sein(key: Int): Sein<T> {
+        //対応するコードをキャッシュから取り出して返す
+        return Sein()
+    }
+
+    val list get(): List<Sein<T>> {
+        //対応するコードをキャッシュから取り出して返す
+        return listOf(Sein(key = 5), Sein(key = 6))
+    }
+}
+
+
+class 歴史的人物 {
+    var 紀年法コード: Int = 1
+    var 誕生年: Date = Date()
+
+    fun 誕生年(): Sein<時間的Enum群.紀年法> {
+        return 時間的Enum群.紀年法.sein(紀年法コード).at(誕生年)
+    }
+    fun 西暦誕生年(): Sein<時間的Enum群.紀年法> {
+        return 時間的Enum群.紀年法.西暦.sein.at(誕生年)
+    }
+}
